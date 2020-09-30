a4d05e676f8f6a4c6c20636876f1bf1ba098ebc3
Bruno Martins <brunomartins56822@gmail.com>
2020-09-30 02:52:18 -0300

    Update

---
+++ src/components/global/Head.js
@@ -0,0 +1,9 @@
+import Head from "next/head";
+export default function Head() {
+  return (
+    <Head>
+      <title>HakaPlay</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
